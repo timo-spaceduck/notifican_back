fe42e12c56bf30744f3c161f7fc1439db3b02317
Timo <timofey.khmelev@gmail.com>
2025-11-04 23:01:46 +0500

    sitemap

--- src/services/cors.service.js
+++ src/services/cors.service.js
@@ -11,11 +11,12 @@ const allowedOrigins = [
 
 const corsOptions = {
 	origin: function (origin, callback) {
-		if (!origin || allowedOrigins.includes(origin)) {
-			callback(null, true);
-		} else {
-			callback(new Error('Not allowed by CORS'));
-		}
+		callback(null, true);
+		// if (!origin || allowedOrigins.includes(origin)) {
+		// 	callback(null, true);
+		// } else {
+		// 	callback(new Error('Not allowed by CORS'));
+		// }
 	},
 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 	allowedHeaders: ['Content-Type', 'Authorization'],
